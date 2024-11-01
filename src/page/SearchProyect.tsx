e7d39e3390f468d678825b949bf55b60923a1a55
sebasrpg1232 <sebasrpg1232@gmail.com>
2024-11-01 14:42:34 -0500

    bugfixed

--- src/page/SearchProyect.tsx
+++ src/page/SearchProyect.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { Search, Plus, Home, Filter } from 'lucide-react'
+import { Search, Plus, Home } from 'lucide-react'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
